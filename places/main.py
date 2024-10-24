f3741930cc4ede15d50f9729b2feafc59f70112b
Juan Andrés Avelino Ospina <javelino2311@gmail.com>
2024-10-23 23:51:36 -0500

    Fixes

--- places/main.py
+++ places/main.py
@@ -3,12 +3,16 @@ from fastapi import FastAPI
 import views
 
 def create_app():
-    app = FastAPI()
+    app = FastAPI(
+        docs_url="/places/docs",
+        redoc_url=None,
+    )
     
-    app.include_router(views.router, prefix=views.API_PREFIX)
+    app.include_router(views.router)
 
     return app
 
 if __name__ == "__main__":
     app = create_app()
+    # avoid redirects
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- places/models/db.py
+++ places/models/db.py
@@ -2,7 +2,7 @@ import motor.motor_asyncio
 from pydantic.functional_validators import BeforeValidator
 from typing_extensions import Annotated
 
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://monitoring_user:isis2503@10.128.0.87:27017?retryWrites=true&w=majority")
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://monitoring_user:isis2503@10.128.0.86:27017?retryWrites=true&w=majority")
 db = client.get_database("monitoring_db")
 places_collection = db.get_collection("places")
 
